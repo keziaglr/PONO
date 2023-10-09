28377d9f941dc6357de9e826684c33095111862d
Kezia Gloria <86879164+keziaglr@users.noreply.github.com>
2023-10-09 10:55:21 +0700

    fix: letter "b" path

--- macro-challenge/Tracing/LetterB.swift
+++ macro-challenge/Tracing/LetterB.swift
@@ -60,19 +60,28 @@ class LetterB: Letter {
     
     func createDottedLine2() {
         let cgPath = CGMutablePath()
-        
-        //TODO: fix the shape of the curve on b
-        let p0 = CGPoint(x: quarterRatio * 1.5, y: quarterRatio * 2.25)
-        let p1 = CGPoint(x: quarterRatio * 1.5, y: quarterRatio * 3.75)
-        initPoint = p0
-        
-        cgPath.move(to: p0)
-        cgPath.addCurve(to: p1, control1: CGPoint(x: quarterRatio * 3.5, y: quarterRatio * 2.25), control2: CGPoint(x: quarterRatio * 3.5, y: quarterRatio * 3.75))
-        cgPath.move(to: p0)
-        cgPath.closeSubpath()
+        // Replace with your desired value
+        let p0 = CGPoint(x: quarterRatio * 2.05, y: quarterRatio * 2.95)
+        let radius: CGFloat = 60
+
+        // Calculate the starting and ending angles
+        let startAngle: CGFloat = 1.2 * .pi
+        let endAngle: CGFloat = 2.8 * .pi
+
+        // Move the path to the initial point
+        initPoint = CGPoint(x: p0.x + radius * cos(startAngle), y: p0.y + radius * sin(startAngle))
+
+        // Add the arc to the path
+        cgPath.addArc(
+            center: p0,
+            radius: radius,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: false
+        )
         
         insertDottedLayer(step: 2, cgPath: cgPath)
-        insertArrow(start: p0, end: p1)
+        insertArrow(start: CGPoint(x: p0.x + radius * cos(endAngle), y: p0.y + 30 + radius * sin(endAngle)), end: CGPoint(x: p0.x + radius * cos(endAngle), y: p0.y + radius * sin(endAngle)))
     }
     
 }
