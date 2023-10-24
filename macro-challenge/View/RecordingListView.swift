ec63042d1cf2b56e0abb95343daad69eadf8b769
Rio Johanes <72259697+riojohanes@users.noreply.github.com>
2023-10-24 09:52:37 +0700

    fix record + slicing ui

--- macro-challenge/View/RecordingListView.swift
+++ macro-challenge/View/RecordingListView.swift
@@ -8,60 +8,87 @@
 import SwiftUI
 
 struct recordingListView: View {
-
+    
     @ObservedObject var vm = VoiceViewModel()
-
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                ScrollView(showsIndicators: false){
-                    ForEach(vm.recordingsList, id: \.createdAt) { recording in
-                        VStack{
-                            HStack{
-                                Image(systemName:"headphones.circle.fill")
-                                    .font(.system(size:50))
-
-                                VStack(alignment:.leading) {
-                                    Text("\(recording.fileURL.lastPathComponent)")
-                                }
-                                VStack {
-                                    Button(action: {
-                                        vm.deleteRecording(url:recording.fileURL)
-                                    }) {
-                                        Image(systemName:"xmark.circle.fill")
-                                            .foregroundColor(.white)
-                                            .font(.system(size:15))
-                                    }
-                                    Spacer()
-
-                                    Button(action: {
-                                        if recording.isPlaying == true {
-                                            vm.stopPlaying(url: recording.fileURL)
-                                        }else{
-                                            vm.startPlaying(url: recording.fileURL)
-                                        }
-                                    }) {
-                                        Image(systemName: recording.isPlaying ? "stop.fill" : "play.fill")
-                                            .foregroundColor(.white)
-                                            .font(.system(size:30))
-                                    }
-
-                                }
-
-                            }.padding()
-                        }.padding(.horizontal,10)
-                        .frame(width: 370, height: 85)
-                        .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
-                        .cornerRadius(30)
-                        .shadow(color: Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)).opacity(0.3), radius: 10, x: 0, y: 10)
-                    }
+        VStack {
+            HStack {
+                Button(action: {
+                    
+                }) {
+                    Image(systemName: "gear")
+                        .foregroundColor(.black)
+                        .font(.system(size: 67, weight: .bold))
                 }
-
-            }.padding(.top,30)
-            .navigationBarTitle("Recordings")
+                
+                Spacer()
+                
+                Rectangle()
+                    .frame(width: 830, height: 20)
+                
+                Spacer()
+                
+                Button(action: {
+                    if vm.isRecording == true {
+                        vm.stopRecording()
+                    }
+                    vm.fetchAllRecording()
+                }) {
+                    Image(systemName: "questionmark.app")
+                        .foregroundColor(.black)
+                        .font(.system(size: 67, weight: .bold))
+                }
+            }.padding(20)
+            Spacer()
         }
+        ScrollView(showsIndicators: false){
+            ForEach(vm.recordingsList, id: \.createdAt) { recording in
+                VStack{
+                    HStack{
+                        Image(systemName:"headphones.circle.fill")
+                            .font(.system(size:50))
+                        
+                        VStack(alignment:.leading) {
+                            Text("\(recording.fileURL.lastPathComponent)")
+                        }
+                        VStack {
+                            Button(action: {
+                                vm.deleteRecording(url:recording.fileURL)
+                            }) {
+                                Image(systemName:"xmark.circle.fill")
+                                    .foregroundColor(.white)
+                                    .font(.system(size:15))
+                            }
+                            Spacer()
+                            
+                            Button(action: {
+                                if recording.isPlaying == true {
+                                    vm.stopPlaying(url: recording.fileURL)
+                                }else{
+                                    vm.startPlaying(url: recording.fileURL)
+                                }
+                            }) {
+                                Image(systemName: recording.isPlaying ? "stop.fill" : "play.fill")
+                                    .foregroundColor(.white)
+                                    .font(.system(size:30))
+                            }
+                            
+                        }
+                        
+                    }.padding()
+                }.padding(.horizontal,10)
+                    .frame(width: 370, height: 85)
+                    .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+                    .cornerRadius(30)
+                    .shadow(color: Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)).opacity(0.3), radius: 10, x: 0, y: 10)
+            }
+        }.padding(.top,30)
+            .navigationBarTitle("Recordings")
     }
 }
+
+
 
 struct recordingListView_Previews: PreviewProvider {
     static var previews: some View {
--- macro-challenge/View/RecordingView.swift
+++ macro-challenge/View/RecordingView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct RecordingView: View {
-
+    
     @ObservedObject var vm = VoiceViewModel()
-
+    
     @State private var showingList = false
-
+    
     var body: some View {
-        NavigationView{
-            ZStack{
-                VStack{
+        ZStack{
+            VStack{
+                HStack {
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName: "gear")
+                            .foregroundColor(.black)
+                            .font(.system(size: 67, weight: .bold))
+                    }
+                    
+                    Spacer()
+                    
+                    Rectangle()
+                        .frame(width: 830, height: 20)
+                    
+                    Spacer()
+                    
                     Button(action: {
                         if vm.isRecording == true {
                             vm.stopRecording()
@@ -24,59 +39,59 @@ struct RecordingView: View {
                         vm.fetchAllRecording()
                         showingList.toggle()
                     }) {
-                        Image(systemName: "list.bullet")
+                        Image(systemName: "questionmark.app")
                             .foregroundColor(.black)
-                            .font(.system(size: 20, weight: .bold))
+                            .font(.system(size: 67, weight: .bold))
                     }.sheet(isPresented: $showingList, content: {
                         recordingListView()
                     })
-
-                    Spacer()
-
-                    if vm.isRecording {
-                        VStack(alignment : .leading , spacing : -5){
-                            HStack (spacing : 3) {
-                                Image(systemName: vm.isRecording && vm.toggleColor ? "circle.fill" : "circle")
-                                    .font(.system(size:10))
-                                    .foregroundColor(.red)
-                                Text("Rec")
-                            }
-                            Text(vm.timer)
-                                .font(.system(size:60))
-                                .foregroundColor(.black)
-                        }
-                    } else {
-                        VStack{
-                            Text("Press the Recording Button below")
-                                .foregroundColor(.black)
-                            Text("and Stop when its done")
-                                .foregroundColor(.black)
-                        }.frame(width: 300, height: 100, alignment: .center)
-                    }
-                    ZStack {
-                        Image(systemName: vm.isRecording ? "stop.circle.fill" : "mic.circle.fill")
-                            .foregroundColor(.black)
-                            .font(.system(size: 45))
-                            .onTapGesture {
-                                if vm.isRecording == true {
-                                    vm.stopRecording()
-                                } else {
-                                    vm.startRecording()
-
-                                }
-                            }
-                    }
-
-                    Spacer()
-
                 }
-                .padding(.leading,25)
-                .padding(.trailing,25)
-                .padding(.top , 70)
-            }.navigationTitle("Voice Record")
-        }
+                
+                Spacer()
+                
+                Spacer()
+                
+                if vm.isRecording {
+                    VStack(alignment : .leading , spacing : -5){
+                        HStack (spacing : 3) {
+                            Image(systemName: vm.isRecording && vm.toggleColor ? "circle.fill" : "circle")
+                                .font(.system(size:10))
+                                .foregroundColor(.red)
+                            Text("Rec")
+                        }
+                        Text(vm.timer)
+                            .font(.system(size:60))
+                            .foregroundColor(.black)
+                    }
+                } else {
+                    VStack{
+                        Text("Apakah kamu puas dengan hasil suaramu?")
+                            .foregroundColor(.black)
+                    }.frame(width: 300, height: 100, alignment: .center)
+                }
+                    Image(systemName: vm.isRecording ? "stop.circle.fill" : "mic.circle.fill")
+                        .foregroundColor(.black)
+                        .font(.system(size: 45))
+                        .onTapGesture {
+                            if vm.isRecording == true {
+                                vm.stopRecording()
+                            } else {
+                                vm.startRecording()
+                                
+                            }
+                        }
+                
+                
+                Spacer()
+                
+            }
+            .padding(.leading,25)
+            .padding(.trailing,25)
+            .padding(.top , 70)
+        }.navigationTitle("Voice Record")
     }
 }
+
 
 struct RecordingView_Previews: PreviewProvider {
     static var previews: some View {
