15f481d84c138a7999f8bb2a4db5eeaf45e368d0
Kezia Gloria <86879164+keziaglr@users.noreply.github.com>
2023-11-03 21:46:44 +0700

    add: new audio assets

--- macro-challenge/Presentation/Modules/Break Word Activity/BreakWordViewModel.swift
+++ macro-challenge/Presentation/Modules/Break Word Activity/BreakWordViewModel.swift
@@ -2,19 +2,7 @@
 //  BreakWordViewModel.swift
 //  macro-challenge
 //
-//  Created by Kezia Gloria on 02/11/23.
+//  Created by Kezia Gloria on 03/11/23.
 //
 
-import SwiftUI
-
-struct BreakWordViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct BreakWordViewModel_Previews: PreviewProvider {
-    static var previews: some View {
-        BreakWordViewModel()
-    }
-}
+import Foundation
